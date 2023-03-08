2261c6dc6996a81da80feb73d6a26a8c071770c0
AndyLammerts <124131@outlook.com>
2023-03-08 12:58:55 +0100

    Gezorgd voor slash commands

---
+++ commands/awaitingTasks.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('awaiting_tasks')
+		.setDescription('Een command om te testen'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
--- src/bot.js
+++ src/bot.js
@@ -1,32 +1,13 @@
 require('dotenv').config()
 
+const fs = require('node:fs');
+const path = require('node:path');
+
 const express = require("express");
 const app = express()
-// var mysql = require('mysql');
-
-// var con = mysql.createConnection({
-//   host: "proron-wordpress.db.transip.me",
-//   user: "proron_staging",
-//   password: "Proron@MM3B"
-// });
-
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
-// con.end();
-
-
-app.listen(3000, () => {
-  console.log("Project is running")
-})
-
-app.get("/", (req, res) => {
-  res.send("Hello world");
-})
 
 const Discord = require("discord.js");
-const { GatewayIntentBits, MessageEmbed } = require("discord.js");
+const { Client, Collection, Events, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 const client = new Discord.Client({ 
     intents: [
         GatewayIntentBits.DirectMessages,
@@ -35,30 +16,72 @@ const client = new Discord.Client({
         GatewayIntentBits.MessageContent
 
     ]});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, '../commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+//Wat er gebeurt als er een bericht in de chat gestuurd wordt. Eerst kijken we of het van de bot komt, daarna of het een command is.
 client.on("messageCreate", message => {
   if (message.author.id != '1082615160365137971') {
     console.log("=======BEGIN BERICHT========");
     console.log(message)
-    if (message.content === "Karim") {
-      message.channel.send("Houd je kenker snavel dicht vieze kut egyptenaar")
+  
+    if (message.content.toLowerCase().includes("brave terry")) {
+      message.channel.send("DANKUWEL");
     }
-    if (message.content === "Terry") {
-        message.channel.send("IK BEN TEWWY")
-      }
     if (message.content === "embed") {
-      let embed = new Discord.EmbedBuilder()
+      let embed = new EmbedBuilder()
         .setTitle("Probleem oplossen")
         .setURL("https://staging.proron-werkkleding.nl/")
-        .addField("Naam van de klant", name)
-        .addField("Token van de klant", "token klant")
-        .addField("Titel van het probleem", "probleem titel")
-        .addField("Description van het probleem", "description titel")
-
+        .addFields(
+          {name: "Naam van de klant", value: 'test'},
+          {name: "Token van de klant", value: "token klant"},
+          {name: "Titel van het probleem", value: "probleem titel"},
+          {name: "Description van het probleem", value: "description titel"},
+          )
       message.channel.send({ embeds: [embed] })
     }
-    console.log("=======EINDE BERICHT========");
 
+    console.log("=======EINDE BERICHT========");
   }
 })
 
 client.login(process.env.token)
+
+app.listen(3000, () => {
+  console.log("Terry bot is actief.")
+})
